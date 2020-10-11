4da85aa31e7dea3a2e8acee6014a21db6a3e4791
feldo <filipcayka@gmail.com>
2020-10-11 16:27:53 +0200

    set title

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -3,11 +3,13 @@
 #include <classes.h>
 #include <windows.h>
 #include <conio.h>
+#include <tchar.h>
 
 using namespace std;
 
 int main()
 {
+    SetConsoleTitle(_T("Simple calculator"));
 	pl numbs;
     char op;
 	cout << "Enter numbers: ";
