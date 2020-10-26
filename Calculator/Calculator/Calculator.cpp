37f04e33b58f044460fd26928c96098b590b7ba3
feldo <filipcayka@gmail.com>
2020-10-26 02:22:05 +0100

    removed using namespace std
    and added something for my future projects lol

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -1,55 +1,51 @@
-//version 1.3
-#include <iostream>
+//version 1.5
+//My custom header files
 #include <classes.h>
+#include <Functions.h>
+
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
 #include <tchar.h>
 
-
-using namespace std;
-
-int IniGui() {
-    cout << "fish" << "\n";
-    _getch();
-    return 0;
+//this will wait
+void IniGui() {
+    std::cout << "fish" << "\n";
+    getInput();
 }
-
-
+//still learning c++ soooooo...........
 void calcNogui() {
     SetConsoleTitle(_T("Simple calculator"));
     pl numbs;
     char op;
-    cout << "Enter numbers: ";
-    cin >> numbs.num1 >> numbs.num2;
-    cout << "Your entered numbers : " << numbs.num1 << " and " << numbs.num2 << "\n";
-    cout << "Enter operator: +, -, * or /: \n";
-    cin >> op;
+    std::cout << "Enter numbers: ";
+    std::cin >> numbs.num1 >> numbs.num2;
+    std::cout << "Your entered numbers : " << numbs.num1 << " and " << numbs.num2 << "\n";
+    std::cout << "Enter operator: +, -, * or /: \n";
+    std::cin >> op;
     switch (op)
     {
     case '+':
-        cout << numbs.num1 + numbs.num2 << "\n";
+        std::cout << numbs.num1 + numbs.num2 << "\n";
         break;
 
     case '-':
-        cout << numbs.num1 - numbs.num2 << "\n";
+        std::cout << numbs.num1 - numbs.num2 << "\n";
         break;
 
     case '*':
-        cout << numbs.num1 * numbs.num2 << "\n";
+        std::cout << numbs.num1 * numbs.num2 << "\n";
         break;
 
     case '/':
-        cout << numbs.num1 / numbs.num2 << "\n";
+        std::cout << numbs.num1 / numbs.num2 << "\n";
         break;
 
     default:
-        cout << "Error! operator is not correct" << "\n";
+        std::cout << "Error! operator is not correct" << "\n";
         break;
     }
-    // you like bread 
-    cout << "Press any key to continue...";
-    _getch();
-    return;
+    getInput();
 }
 
 int main()
@@ -62,6 +58,6 @@ int main()
     else {
         calcNogui();
     }
-
+    return 0;
 }
 
